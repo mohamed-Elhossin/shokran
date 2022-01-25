76e48b7887d99f91fe82e85fc827e524849891f9
Toma <toma@Tomas-MacBook-Pro.local>
2022-01-25 11:01:49 +0200

    new Update

---
+++ assets/js/myjs.js
@@ -0,0 +1,14 @@
+
+
+
+let iconBox = document.getElementsByClassName('icon-box');
+
+
+iconBox[1].style.backgroundImage = 'url(./assets/img/serv/p1.png)';
+iconBox[2].style.backgroundImage = 'url(./assets/img/serv/p2.png)';
+iconBox[3].style.backgroundImage = 'url(./assets/img/serv/p3.png)';
+iconBox[4].style.backgroundImage = 'url(./assets/img/serv/p4.png)';
+iconBox[5].style.backgroundImage = 'url(./assets/img/serv/p5.png)';
+iconBox[6].style.backgroundImage = 'url(./assets/img/serv/p6.png)';
+iconBox[7].style.backgroundImage = 'url(./assets/img/serv/p9.png)';
+iconBox[8].style.backgroundImage = 'url(./assets/img/serv/p8.png)';
